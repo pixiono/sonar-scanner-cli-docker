04363e13c1244863001a4ca549e3ffb4f8eb6f91
Michal Duda <michal.duda@sonarsource.com>
2020-05-25 11:50:41 +0200

    fixup! SQSCANNER-77 fix permission related issues when using Docker image

--- 4/bin/entrypoint.sh
+++ 4/bin/entrypoint.sh
@@ -21,25 +21,21 @@ if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
 fi
 
 if [[ "$1" = 'sonar-scanner' ]]; then
-    chown -R "$(id -u):$(id -g)" "${PWD}" "${SONAR_USER_HOME}" 2>/dev/null || :
-    chmod -R 700 "${PWD}" "${SONAR_USER_HOME}" 2>/dev/null || :
-
-    # Allow the container to be started with `--user`
-    if [[ "$(id -u)" = '0' ]]; then
-        chown -R scanner-cli:scanner-cli "${PWD}" "${SONAR_USER_HOME}"
-        exec su-exec scanner-cli "$0" "$@"
-    fi
-
-  if mkdir -p "${SONAR_USER_HOME}/.sonar" > /dev/null 2>&1 ; then
-    add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
-    add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
-    add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
-    if [ ${#args[@]} -ne 0 ]; then
-      set -- sonar-scanner "${args[@]}" "${@:2}"
-    fi
-  else
-    echo "Cannot write to ${SONAR_USER_HOME}/.sonar. Please make sure you have set appriopriate permissions for all used volumes."
+  if [[ ! -r "${SONAR_USER_HOME}" ]] || [[ ! -w "${SONAR_USER_HOME}" ]]; then
+    echo "Cannot read from or write to '${SONAR_USER_HOME}'. Please make sure appropriate permissions for this volume are set."
     exit 1
+  fi
+
+  if [[ ! -r "${PWD}" ]] || [[ ! -w "${PWD}" ]]; then
+    echo "Cannot read from or write to '${PWD}'. Please make sure appropriate permissions are set for volumes used."
+    exit 1
+  fi
+
+  add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
+  add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
+  add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
+  if [ ${#args[@]} -ne 0 ]; then
+    set -- sonar-scanner "${args[@]}" "${@:2}"
   fi
 fi
 
